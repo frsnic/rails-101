824dca3e84e92396a6d125e96802e4a54305dbb9
Peter <peter.pan@photomugs.com>
2015-09-09 06:29:12 +0000

    add topic welocme action & setting root page

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -61,6 +61,9 @@ class TopicsController < ApplicationController
     end
   end
 
+  def welcome
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_topic
