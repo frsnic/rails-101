ca12b51fc19dbdd66e1879faec2d6c08b067929e
Peter <peter.pan@photomugs.com>
2015-09-10 08:25:16 +0000

    create account/group account/post resource

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ActiveRecord::Base
   validates :title, presence: { message: '不可空白' }
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   belongs_to :owner, class_name: "User", foreign_key: :user_id
 
---
+++ db/migrate/20150910081601_add_posts_count_to_group.rb
@@ -0,0 +1,8 @@
+class AddPostsCountToGroup < ActiveRecord::Migration
+
+  def change
+    add_column :groups, :posts_count, :integer, default: 0
+  end
+
+end
+
